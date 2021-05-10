8f9597190b0c42df361a4293c44554b15fedfc8a
Niklas Appelquist <niklasappelquist@gmail.com>
2021-05-10 07:04:36 +0200

    change file structure

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+
+
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivites' />
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
