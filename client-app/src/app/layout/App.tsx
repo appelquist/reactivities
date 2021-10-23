bb60fa4e2e51acc52f41ec4e409417a0b17b5200
Niklas Appelquist <niklasappelquist@gmail.com>
2021-10-23 09:34:13 +0200

    Add key to activity form route to fix wrong behaviour when clicking on create activity while editing an activity

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashBoard from '../../features/activities/dashboard/ActivityDashboard';
-import { Route } from 'react-router';
+import { Route, useLocation } from 'react-router';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
+  const location = useLocation();
   return (
     <>
       <NavBar />
@@ -15,7 +16,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route exact path="/activities" component={ActivityDashBoard} />
         <Route path="/activities/:id" component={ActivityDetails} />
-        <Route path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
+        <Route key={location.key} path={["/createActivity", "/manage/:id"]} component={ActivityForm} />
       </Container>
     </>
   );
